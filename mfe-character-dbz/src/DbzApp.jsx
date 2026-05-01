19904710332d0983f0868184848fae019794efc8
FrancoDavid <franco.avalos@live.com>
2026-04-30 22:40:14 -0400

    feat: table de products

--- mfe-character-dbz/src/DbzApp.jsx
+++ mfe-character-dbz/src/DbzApp.jsx
@@ -1,4 +1,7 @@
-import { Card, CardContent, Grid, Typography } from "@mui/material";
+import { Box, Card, CardContent, Grid, Typography } from "@mui/material";
+import { getProducts } from "./services/services";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function DbzApp() {
   // const handleAttackClick = () => {
@@ -6,20 +9,47 @@ function DbzApp() {
   //   window.dispatchEvent(new CustomEvent("battle:attack-player-one"));
   // };
 
-  // useEffect(() => {
-  //   getCharacter().then((character) => setCharacter(character));
-  // }, []);
+  const [products, setProducts] = useState();
+
+  useEffect(() => {
+    getProducts().then((products) => {
+      setProducts(products);
+    });
+  }, []);
 
   return (
-    <Grid container>
-      <Grid item xs={12}>
-        <Card
-          sx={{
-            bgcolor: "red",
-          }}
-        >
+    <Grid container sx={{ padding: "20px" }}>
+      <Grid item xs={12} sx={{ padding: "20px" }}>
+        <Card sx={{ bgcolor: "white" }}>
           <CardContent>
-            <Typography>Hola aca prodyctos</Typography>
+            <Typography
+              variant="h5"
+              component="h3"
+              sx={{ margin: "20px 0 20px" }}
+            >
+              Jimmy's
+            </Typography>
+            {products?.map((product) => (
+              <Grid container spacing={3} sx={{ margin: "20px 0 8px" }}>
+                <Grid item xs={6}>
+                  <Box
+                    component="img"
+                    src={product.image}
+                    alt="Descripción"
+                    sx={{ width: "30px", height: "auto" }}
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography
+                    variant="body1"
+                    sx={{ fontWeight: "bold", textWrap: "wrap" }}
+                  >
+                    {product.title}
+                  </Typography>
+                  <Typography variant="body2">{product.category}</Typography>
+                </Grid>
+              </Grid>
+            ))}
           </CardContent>
         </Card>
       </Grid>
